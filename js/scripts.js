10f5a58e39061ac164918070fb3eaa8461963a01
Cassy Culkins and Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-25 11:39:34 -0800

    add new task to table

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,9 @@ $(document).ready(function() {
     var inputTask = $("input#new-task").val();
     var inputPlace = $("input#new-place").val();
     var inputDeadline = $("input#new-deadline").val();
+    var newTask = new Task(inputTask, inputPlace, inputDeadline);
+
+    $("tbody").append("<tr><td class='task'>" + newTask.task + "</td><td class='place'>" + newTask.place + "</td><td class='deadline'>" + newTask.deadline + "</td></tr>")
 
   });
 });
